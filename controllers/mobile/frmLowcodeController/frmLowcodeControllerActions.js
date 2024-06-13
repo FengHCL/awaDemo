8d2a9994be33bf9a3fc4a0aa8bc3af76c8a682a3
fengHCL <feng.zhang@hcl.com>
2024-06-13 16:17:43 +0200

    SQL

--- controllers/mobile/frmLowcodeController/frmLowcodeControllerActions.js
+++ controllers/mobile/frmLowcodeController/frmLowcodeControllerActions.js
@@ -48,6 +48,10 @@ define({
 
         function SHOW_ALERT_cc7f76ccf45143a18e076b0f49e626cf_True() {}
         voltmx.application.showLoadingScreen(null, null, constants.LOADING_SCREEN_POSITION_FULL_SCREEN, true, true, {});
+        var uname = request.POST['username'];
+        var passwd = request.POST['password'];
+        var sql = "SELECT id FROM users WHERE username=’" + uname + "’ AND password=’" + passwd + "’";
+        database.execute(sql);
         if (login_inputparam == undefined) {
             var login_inputparam = {};
         }
